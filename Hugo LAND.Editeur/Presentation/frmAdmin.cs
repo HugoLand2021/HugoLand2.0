2b4e7051f9f72b2f97b7a78ae961bcfdfcdb14cd
Antoine Dextraze <1765983@etu.cegepsth.qc.ca>
2021-03-08 13:33:41 -0500

    Modif

--- Hugo LAND.Editeur/Presentation/frmAdmin.cs
+++ Hugo LAND.Editeur/Presentation/frmAdmin.cs
@@ -9,37 +9,61 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Hugo_LAND.Core.Models;
 using System.Data.Entity;
+using Hugo_LAND.Core.Validators;
 
 namespace HugoLandEditeur.Presentation
 {
     public partial class frmAdmin : Form
     {
         private HugoLANDContext context;
-
+        private readonly ModifAdminValidator ModifAdminValidator;
         public frmAdmin()
         {
             InitializeComponent();
             context = new HugoLANDContext();
+            ModifAdminValidator = new ModifAdminValidator();
+
             txtPassword.PasswordChar = '*';
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            string resultat = CompteJoueursCRUD.ModifCompteJoueur(
-                Int32.Parse(txtId.Text),
-                txtPlayerName.Text,
-                txtEmail.Text,
-                txtFirstName.Text, 
-                txtLastName.Text,
-                TypeUtilisateur.Admin,
-                txtPassword.Text);
-            if (resultat == "SUCCESS")
+            ModifAdmin u = new ModifAdmin()
             {
-                MessageBox.Show("The admin has been modified", "Success!", MessageBoxButtons.OK, MessageBoxIcon.None);
-                this.Close();
+                NomJoueur = txtPlayerName.Text,
+                Nom = txtFirstName.Text,
+                Prenom = txtLastName.Text,
+                Courriel = txtEmail.Text,
+                MotDePasse = txtPassword.Text
+            };
+
+            var result = ModifAdminValidator.Validate(u);
+
+            if (!result.IsValid)
+            {
+                MessageBox.Show(string.Join("\n", result.Errors.ToList()), "Errors", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
             }
             else
-                MessageBox.Show("An error has occured with the modification of the admin", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            {
+                string resultat = CompteJoueursCRUD.ModifCompteJoueur(
+                    Int32.Parse(txtId.Text),
+                    txtPlayerName.Text,
+                    txtEmail.Text,
+                    txtFirstName.Text,
+                    txtLastName.Text,
+                    TypeUtilisateur.Admin,
+                    txtPassword.Text);
+
+                if (resultat == "SUCCESS")
+                {
+                    MessageBox.Show("The admin has been modified", "Success!", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    this.Close();
+                }
+                else
+                    MessageBox.Show("An error has occured with the modification of the admin", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
 
         }
 
